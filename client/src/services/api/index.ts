1da4c06cd1b7066e279fecf8b39ef93411cf3103
HEECHANG96 <wjdgmlckd96@daum.net>
2023-07-28 23:29:59 +0900

    chore: 파일 구조 세팅 및 예시 코드 작성, axios, zustand devtools 설정 추가

---
+++ client/src/services/api/index.ts
@@ -0,0 +1,76 @@
+import axios, { Method } from "axios";
+
+export let axiosClient = axios.create({
+  baseURL: "http://localhost:4000",
+});
+
+axiosClient.defaults.withCredentials = true;
+
+class API {
+  async CALL({
+    method,
+    url,
+    data = null,
+  }: {
+    method: Method;
+    url: string;
+    data?: any;
+  }) {
+    try {
+      const response = await axiosClient({
+        url,
+        method,
+        data,
+      });
+
+      return response;
+    } catch (error: any) {
+      if (error.response && error.response.status === 401) {
+      }
+
+      console.error("# client-error-axios: ", error);
+      return error.response;
+    }
+  }
+
+  GET(url: string) {
+    return this.CALL({
+      method: "GET",
+      url,
+    });
+  }
+
+  POST({ url, ...params }: { url: string; [key: string]: any }) {
+    return this.CALL({
+      method: "POST",
+      url,
+      ...params,
+    });
+  }
+
+  PUT({ url, ...params }: { url: string; [key: string]: any }) {
+    return this.CALL({
+      method: "PUT",
+      url,
+      ...params,
+    });
+  }
+
+  DELETE({ url, ...params }: { url: string; [key: string]: any }) {
+    return this.CALL({
+      method: "DELETE",
+      url,
+      ...params,
+    });
+  }
+
+  PATCH({ url, ...params }: { url: string; [key: string]: any }) {
+    return this.CALL({
+      method: "PATCH",
+      url,
+      ...params,
+    });
+  }
+}
+
+export default new API();
