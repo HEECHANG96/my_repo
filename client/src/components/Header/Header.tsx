23f07f6aa115b9f3b3fde3f48e5175d9c964f3b8
HEECHANG96 <wjdgmlckd96@daum.net>
2023-07-30 23:41:49 +0900

    style: 헤더 구조 구현

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,15 +1,25 @@
 import styled from 'styled-components';
+import { BsSearch, BsPersonCircle } from 'react-icons/bs';
+import { Input } from 'antd';
 
 const Header = () => {
   return (
     <Container>
       <Title>오늘의 여행</Title>
-      <div>
+      <MenuDiv>
         <span>홈</span>
         <span>지역</span>
-      </div>
-      <div>검색창</div>
-      <div>마이페이지 아이콘</div>
+      </MenuDiv>
+      <SearchDiv>
+        <SearchInput
+          placeholder="가고 싶은 지역을 검색해보세요"
+          prefix={<BsSearch />}
+        ></SearchInput>
+      </SearchDiv>
+
+      <MypageIconDiv>
+        <SPersonCircle />
+      </MypageIconDiv>
     </Container>
   );
 };
@@ -28,6 +38,48 @@ const Container = styled.div`
 `;
 
 const Title = styled.div`
+  width: 100%;
   font-size: 28px;
   font-weight: 600;
+  margin-left: 3rem;
+  cursor: pointer;
+`;
+
+const MenuDiv = styled.div`
+  width: 100%;
+
+  span {
+    margin-right: 1rem;
+    font-weight: 500;
+    cursor: pointer;
+
+    &:hover {
+      border-bottom: 1px solid gray;
+    }
+
+    &:active {
+      border-bottom: 1px solid gray;
+    }
+  }
+`;
+
+const SearchDiv = styled.div`
+  width: 100%;
+`;
+const SearchInput = styled(Input)`
+  width: 250px;
+  border: none;
+  border-bottom: 1px solid gray;
+`;
+
+const MypageIconDiv = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: end;
+`;
+
+const SPersonCircle = styled(BsPersonCircle)`
+  font-size: 24px;
+  margin-right: 3rem;
+  cursor: pointer;
 `;
