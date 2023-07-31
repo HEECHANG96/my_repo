1b96389f3b8e71b47fb4c667ee5b960f7c19ab06
HEECHANG96 <wjdgmlckd96@daum.net>
2023-07-31 13:49:46 +0900

    style: 헤더 구조 구현

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,47 +1,50 @@
 import styled from 'styled-components';
 import { BsSearch, BsPersonCircle } from 'react-icons/bs';
 import { Input } from 'antd';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <Container>
-      <Title>오늘의 여행</Title>
+    <SContainer>
+      <LinkTitle to="/">오늘의 여행</LinkTitle>
+
       <MenuDiv>
         <span>홈</span>
         <span>지역</span>
+        <span>숙박</span>
       </MenuDiv>
+
       <SearchDiv>
         <SearchInput
           placeholder="가고 싶은 지역을 검색해보세요"
           prefix={<BsSearch />}
         ></SearchInput>
-      </SearchDiv>
 
-      <MypageIconDiv>
-        <SPersonCircle />
-      </MypageIconDiv>
-    </Container>
+        <PersonCircle />
+      </SearchDiv>
+    </SContainer>
   );
 };
 
 export default Header;
 
-const Container = styled.div`
+const SContainer = styled.div`
   width: 100%;
   height: 100px;
   background-color: #e3efff;
   box-sizing: border-box;
-  border: 1px solid blue;
   display: flex;
   justify-content: space-between;
   align-items: center;
 `;
 
-const Title = styled.div`
+const LinkTitle = styled(Link)`
   width: 100%;
   font-size: 28px;
   font-weight: 600;
   margin-left: 3rem;
+  color: black;
+  text-decoration: none;
   cursor: pointer;
 `;
 
@@ -49,7 +52,8 @@ const MenuDiv = styled.div`
   width: 100%;
 
   span {
-    margin-right: 1rem;
+    margin-right: 2rem;
+    font-size: 22px;
     font-weight: 500;
     cursor: pointer;
 
@@ -65,20 +69,18 @@ const MenuDiv = styled.div`
 
 const SearchDiv = styled.div`
   width: 100%;
+  display: flex;
+  justify-content: end;
+  align-items: center;
 `;
 const SearchInput = styled(Input)`
   width: 250px;
   border: none;
   border-bottom: 1px solid gray;
+  margin-right: 2rem;
 `;
 
-const MypageIconDiv = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: end;
-`;
-
-const SPersonCircle = styled(BsPersonCircle)`
+const PersonCircle = styled(BsPersonCircle)`
   font-size: 24px;
   margin-right: 3rem;
   cursor: pointer;
